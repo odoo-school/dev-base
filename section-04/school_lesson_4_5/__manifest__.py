ecb14961fff9bada5440a729f4b298446ff16d2e
Yurii Razumovskyi <GarazdCreation@gmail.com>
2024-03-08 18:30:14 +0200

    [ADD] school_lesson_4_5

---
+++ section-04/school_lesson_4_5/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright © 2024 Garazd Creation (https://garazd.biz)
+# @author: Yurii Razumovskyi (school@garazd.biz)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'School Lesson 4-5: Search Panel and Views',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': """Odoo School Lesson 4-5: Search Views, Search Panel.""",
+    'license': 'LGPL-3',
+    'author': 'Garazd Creation',
+    'website': 'https://garazd.biz',
+    'depends': [
+        'school_lesson_4_4',
+    ],
+    'data': [
+        'views/social_subscriber_views.xml',
+    ],
+    'support': 'school@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
